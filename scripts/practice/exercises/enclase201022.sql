c8392f93c4814aede32ab2919518e54948f8eb9a
flechaVivas <facundovivas15@gmail.com>
2022-10-26 16:01:29 -0300

    ejs en clase

---
+++ scripts/practice/exercises/enclase201022.sql
@@ -0,0 +1,11 @@
+
+
+-- Crear un SP que dado un porcent de act de costos y una fecha a partir de la cual tiene vigencia el costo
+-- , actualice la tabla de costos
+
+call actualiza_costos(300.0, '20241231');
+
+
+
+
+
